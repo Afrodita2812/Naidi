a9037dc55345ef29218daa1f46440f315541150a
Afrodita2812 <mabelsalomeparrachaverra@gmail.com>
2025-02-10 01:13:49 -0500

    Add files via upload

---
+++ KeraNaidi.API/Program.cs
@@ -0,0 +1,54 @@
+using System.Runtime.CompilerServices;
+using KeraNaidi.Data;
+using KeraNaidi.Interfaces;
+using KeraNaidi.Services;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddTransient<IHealthService, HealthService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+// builder.Services.AddDbContext<KeraNaidiContext>(
+//     opt => opt.UseInMemoryDatabase("KeiraNaidi")
+// );
+builder.Services.AddDbContext<KeraNaidiContext>(
+    opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("KeraNaidi"))
+);
+var app = builder.Build();
+PopulateDb(app);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+    
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
+
+# region Db Populate
+async void PopulateDb(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var healthService = scope.ServiceProvider.GetRequiredService<IHealthService>();
+        await healthService.AddHealthCheck(new KeraNaidi.Data.Models.HealthCheck{
+            Date = DateTime.Now,
+            Message = "DB Check is Complete"
+        });
+        
+    }
+}
+#endregion
---
+++ KeraNaidi.Data/KeiraNaidiContext.cs
@@ -0,0 +1,25 @@
+﻿using KeraNaidi.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeraNaidi.Data;
+
+public class KeraNaidiContext : DbContext
+{
+    public KeraNaidiContext(DbContextOptions<KeraNaidiContext> options) : base(options){
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior",true);
+    }
+
+    public DbSet<HealthCheck> HealthCheck {get;set;}
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        if(modelBuilder == null)
+        {
+            return;
+        }
+        modelBuilder.Entity<HealthCheck>().ToTable("HealthCheck").HasKey(x => x.Id);
+        modelBuilder.Entity<Product>().ToTable("Product").HasKey(x => x.Id);
+        base.OnModelCreating(modelBuilder);
+    }
+}
